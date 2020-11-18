3baaf81d2603c3ced905e676ead8f9bfc80e4522
Koflerj93 <koflerj93@gmail.com>
2020-11-18 02:22:44 -0500

    basic deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //
+  document.querySelector("#create-task-form").addEventListener("submit", postTask);
+
+  function postTask(e) {
+    // prevents a page refresh
+    e.preventDefault();
+    // Gets the value of the from 
+    const value = document.querySelector("#new-task-description").value
+    // Grabs the ul of class tasks
+    const ul = document.querySelector("#tasks")
+    // creates an Li element
+    const newLi = document.createElement("li");
+    // changes the text content of the li to the value value
+    newLi.textContent = value
+    //appends the new li to the ul. 
+    ul.appendChild(newLi)
+  }
+
 });
